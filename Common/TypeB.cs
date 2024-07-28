b21b1caeea17f499c8d28d92ead778b3d1c47eae
[200~LidoOoO~ <lidom899@gmail.com>
2024-07-29 01:09:04 +0300

    Access Modifiers

---
+++ Common/TypeB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    internal class TypeB
+    {
+
+        #region  Access Modifiers
+
+        //// Valid -> TypeB Accessible With its Project ONLY 
+        //public TypeB()
+        //{
+        //    TypeA typeA = new TypeA();
+        //    //// Valid -> TypeA Accessible With its Project
+
+        //    //typeA.X = 10;
+        //    //// X is Private 
+
+        //    typeA.Y = 20; // Valid
+        //    // Y is Internal
+
+        //    typeA.Z = 30; // Valid
+        //    // Z is Public
+
+
+        //}
+
+        #endregion
+
+    }
+}
